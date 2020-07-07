717e2cc6e32e51cf59bc8347f3b636e23d5b3097
dewald <dewalds@africanresonance.com>
2020-07-07 20:24:54 +0200

    PWA tweaks

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 var cacheName = 'mvj';
-var filesToCache = [  
+var filesToCache = [
     './index.html',
     './style.css',
     './js/main.js',
@@ -10,7 +10,41 @@ var filesToCache = [
     './player.js',
     './p5.dom.min.js',
     './p5.min.js',
-    './p5.sound.min.js'
+    './p5.sound.min.js',
+    './assets/ArcadeNormal-ZDZ.ttf',
+    './assets/mvj.jpg',
+    './assets/audio/bomb.mp3',
+    './assets/audio/boss.mp3',
+    './assets/audio/bossdies.mp3',
+    './assets/audio/bullet.mp3',
+    './assets/audio/enemydies.mp3',
+    './assets/audio/playerdies.mp3',
+    './assets/audio/starting.mp3',
+    './assets/audio/victory.mp3',
+    './sprites/faction1/boss1.png',
+    './sprites/faction1/boss2.png',
+    './sprites/faction1/boss3.png',
+    './sprites/faction1/unit1_1.png',
+    './sprites/faction1/unit1_2.png',
+    './sprites/faction1/unit1_3.png',
+    './sprites/faction1/unit2_1.png',
+    './sprites/faction1/unit2_2.png',
+    './sprites/faction1/unit2_3.png',
+    './sprites/faction1/unit3_1.png',
+    './sprites/faction1/unit3_2.png',
+    './sprites/faction1/unit3_3.png',
+    './sprites/faction2/boss1.png',
+    './sprites/faction2/boss2.png',
+    './sprites/faction2/boss3.png',
+    './sprites/faction2/unit1_1.png',
+    './sprites/faction2/unit1_2.png',
+    './sprites/faction2/unit1_3.png',
+    './sprites/faction2/unit2_1.png',
+    './sprites/faction2/unit2_2.png',
+    './sprites/faction2/unit2_3.png',
+    './sprites/faction2/unit3_1.png',
+    './sprites/faction2/unit3_2.png',
+    './sprites/faction2/unit3_3.png'
 ];
 
 /* Start the service worker and cache all of the app's content */
