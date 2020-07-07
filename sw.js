9272a2cf6b419a4b3630b3592e963f26d074ede2
dewald <dewalds@africanresonance.com>
2020-07-07 20:17:06 +0200

    PWA tweaks

--- sw.js
+++ sw.js
@@ -1,6 +1,18 @@
 var cacheName = 'mvj';
 var filesToCache = [  
-    './index.html'
+    './index.html',
+    './style.css',
+    './js/main.js',
+    './sketch.js',
+    './boss.js',
+    './bullet.js',
+    './enemy.js',
+    './player.js',
+    './p5.dom.min.js',
+    './p5.min.js',
+    './p5.sound.min.js',
+    './assets/*',
+    './sprites/*'
 ];
 
 /* Start the service worker and cache all of the app's content */
